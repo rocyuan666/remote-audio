c0e2a87c1d3e688ee17162c9a4d09738be223de8
袁鹏 <rocyuan666@163.com>
2024-04-15 16:25:35 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ function main() {
 
   app.set('view engine', 'ejs')
   app.set('views', path.resolve(__dirname, 'views'))
-  app.use(express.urlencoded())
+  app.use(express.urlencoded({ extended: false }))
   app.use(express.json())
   app.use(express.static(path.resolve(__dirname, 'public')))
 
